417fb9a9ac13803c99813d36f4869230c33e337d
MVergar4 <marcvergara2023@udec.cl>
2024-11-13 22:32:24 -0300

    Añadidas ruta home y book para las reservas

--- flask_web/website/views.py
+++ flask_web/website/views.py
@@ -1,7 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
+
+@views.route('/book')
+def reservas():
+    return render_template("reservas.html")
+
+@views.route('/schedule')
+def horario():
+    return render_template("horario.html")

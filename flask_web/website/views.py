0acdcf93a4eb8b9c43b7a7d46c893a4b2f802429
MVergar4 <marcvergara2023@udec.cl>
2024-11-18 21:41:19 -0300

    Solucion a problema con import

--- flask_web/website/views.py
+++ flask_web/website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, jsonify
-from flask_web.database import *
-from utils import run_valido
+from .database import *
+from .utils import *
 views = Blueprint('views', __name__)
 
 @views.route('/')

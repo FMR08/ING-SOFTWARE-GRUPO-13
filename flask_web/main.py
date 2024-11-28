e45cf940bebeb9174cd52baafa7de1c6e7460510
MVergar4 <marcvergara2023@udec.cl>
2024-11-28 20:48:36 -0300

    Actualizado main.py que estaba repetido

--- flask_web/main.py
+++ flask_web/main.py
@@ -10,15 +10,3 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug = True)
-import sys
-import os
-
-# Añadir la carpeta raíz del proyecto al sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from website import create_app  # Importa después de ajustar el sys.path
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug = True)
